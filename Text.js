f7a35aae760e313219c4ed7cf304f127aea95d39
Munawwar Firoz <munawwarfiroz@hotmail.com>
2014-02-04 22:47:58 +0400

    Replaced SearchField.js with Text.js.

---
+++ Text.js
@@ -0,0 +1,59 @@
+INCLUDE('Component.js');
+
+/**
+ * Text field.
+ */
+Lui.extend('Lui.Text', Lui.Component, {
+    cls: 'form-control',
+    autocomplete: [],
+    placeholder: '',
+    /**
+     * @override
+     */
+    parseLV: function (target) {
+        var cfg = this.super(arguments),
+            node = target.firstChild,
+            placeholder = node.getAttribute('placeholder'),
+            value = node.firstChild.nodeValue;
+        if (placeholder) {
+            cfg.placeholder = placeholder;
+        }
+        if (value) {
+            cfg.value = value;
+        }
+        return cfg;
+    },
+    /**
+     * @override
+     */
+    getHtml: function () {
+        if (!this.tpl) {
+            throw new Error("Can't find template for " + this.type);
+        }
+        return Li.format(this.tpl.text, {
+            id: this.id,
+            type: this.type,
+            cls: this.getCssClass(),
+            style: this.style,
+            value: this.value,
+            placeholder: this.placeholder,
+        });
+    },
+    /**
+     * @override
+     */
+    postRender: function () {
+        this.adjustAutoComplete();
+        this.super(arguments);
+    },
+    /**
+     * @private
+     */
+    adjustAutoComplete: function () {
+        if (this.autocomplete.length) {
+            $(this.rootEl).autocomplete({
+                source: this.autocomplete
+            });
+        }
+    }
+});
