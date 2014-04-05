23a30a6971038935f4543c384dadb99d4eb57541
Munawwar Firoz <munawwarfiroz@hotmail.com>
2014-04-05 11:05:07 +0400

    Updated Htmlizer.

--- core/util/Template.js
+++ core/util/Template.js
@@ -1,5 +1,5 @@
 INCLUDE('util.js');
-INCLUDE('../../lib/Htmlizer/js-object-literal-parse.js');
-INCLUDE('../../lib/Htmlizer/Htmlizer.js');
+INCLUDE('../../lib/Htmlizer/src/js-object-literal-parse.js');
+INCLUDE('../../lib/Htmlizer/src/Htmlizer.js');
 
 Lui.util.Template = Htmlizer;
