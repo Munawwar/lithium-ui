f81e0c282708437fa5406f81b020431dfaee74ad
Munawwar Firoz <munawwarfiroz@hotmail.com>
2014-09-12 19:17:37 +0400

    Refactor: Removed now redundant DOM traversal code.

--- core/Template.js
+++ core/Template.js
@@ -23,7 +23,8 @@ if (typeof define !== 'function') {
     //HTML 4 and 5 void tags
     var voidTags = unwrap('area,base,basefont,br,col,command,embed,frame,hr,img,input,keygen,link,meta,param,source,track,wbr'),
         conflictingBindings = unwrap('if,ifnot,foreach,with,text,html'),
-        customElementRegex = /^(X|L)\-/;
+        customElementRegex = /^(X|L)\-/,
+        traverse = Lui.util.traverse;
 
     /**
      * @param {String|DocumentFragment} template If string, then it is better if the HTML is balanced, else it probably won't be correctly converted to DOM.
@@ -1066,66 +1067,6 @@ if (typeof define !== 'function') {
             })[0] || null;
         }
     };
-
-    /**
-     * Given a DOM node, this method finds the next tag/node that would appear in the dom.
-     * WARNING: Do not remove or add nodes while traversing, because it could cause the traversal logic to go crazy.
-     * @param node Could be a any node (element node or text node)
-     * @param ancestor Node An ancestorial element that can be used to limit the search.
-     * The search algorithm, while traversing the ancestorial heirarcy, will not go past/above this element.
-     * @param {function} callback A callback called on each element traversed.
-     *
-     * callback gets following parameters:
-     * node: Current node being traversed.
-     * isOpenTag: boolean. On true, node would be the next open tag/node that one would find when going
-     * linearly downwards through the DOM. Filtering with isOpenTag=true, one would get exactly what native TreeWalker does.
-     * Similarly isOpenTag=false when a close tag is encountered when traversing the DOM. AFAIK TreeWalker doesn't give this info.
-     *
-     * callback can return one of the following values (with their meanings):
-     * 'halt': Stops immediately and returns null.
-     * 'return': Halts and returns node.
-     * 'continue': Skips further traversal of current node (i.e won't traverse it's child nodes).
-     * 'break': Skips all sibling elements of current node and goes to it's parent node.
-     *
-     * relation: The relation compared to the previously traversed node.
-     * @param {Object} [scope] Value of 'this' keyword within callback
-     * @private
-     */
-    function traverse(node, ancestor, callback, scope) {
-        //if node = ancestor, we still can traverse it's child nodes
-        if (!node) {
-            return null;
-        }
-        var isOpenTag = true, ret = null;
-        do {
-            if (ret === 'halt') {
-                return null;
-            }
-            if (isOpenTag && node.firstChild && !ret) {
-                node = node.firstChild;
-                //isOpenTag = true;
-                ret = callback.call(scope, node, true, 'firstChild');
-            } else if (node.nextSibling && node !== ancestor && ret !== 'break') {
-                if (isOpenTag) {
-                    callback.call(scope, node, false, 'current');
-                }
-                node = node.nextSibling;
-                isOpenTag = true;
-                ret = callback.call(scope, node, true, 'nextSibling');
-            } else if (node.parentNode && node !== ancestor) {
-                if (isOpenTag) {
-                    callback.call(scope, node, false, 'current');
-                }
-                //Traverse up the dom till you find an element with nextSibling
-                node = node.parentNode;
-                isOpenTag = false;
-                ret = callback.call(scope, node, false, 'parentNode');
-            } else {
-                node = null;
-            }
-        } while (node && ret !== 'return');
-        return node || null;
-    }
 
     return Htmlizer;
 }, function () {
