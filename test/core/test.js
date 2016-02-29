43398082c410eb61432631277190ecbb09765748
Munawwar <munawwarfiroz@hotmail.com>
2016-02-29 19:15:07 +0400

    Added tests for custom element.

--- test/core/test.js
+++ test/core/test.js
@@ -9,18 +9,47 @@ requirejs(['core/Box'], function (Li) {
             df = document.createDocumentFragment();
         box.render(df);
 
-        //Remove text nodes
         var el = box.el;
         it('should have root element', function () {
             assert.equal(el, df.firstChild);
         });
 
         it('test component markup', function () {
+            //Remove text nodes
             if (el.firstChild.nodeType === 3) {
                 el.normalize();
                 el.removeChild(el.firstChild);
             }
             assert.equal(utils.toHTML(df), '<div id="cmp-1" data-type="Li.Box" class="li-box"></div>');
+        });
+    });
+
+    var MyComp = Li.extend('MyComp', Li.Box, {
+        innerTpl: new Li.Template(
+            '<li-box ref="innercomp" class="round-box" style="color: red;"></li-box>' +
+            '<li-box params="style: \'color: red;\'"></li-box>'
+        )
+    });
+    describe('test usage of custom element in templates', function () {
+        var cmp = new MyComp({}),
+            df = document.createDocumentFragment();
+        cmp.render(df);
+
+        var el = cmp.el;
+        it('it should have rendered', function () {
+            assert.equal(el.children[0].nodeName, 'DIV');
+        });
+        it('class attribute test', function () {
+            assert.equal(el.children[0].classList[1], 'round-box');
+        });
+        it('style attribute test', function () {
+            assert.equal(el.children[0].style.color, 'red');
+        });
+        it('ref attribute test', function () {
+            assert.equal(true, (cmp.innercomp instanceof Li.Box) && cmp.innercomp.el === el.children[0]);
+        });
+        it('params attribute test', function () {
+            assert.equal(el.children[1].style.color, 'red');
         });
     });
 });
